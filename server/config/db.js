9457726c7164dc9cc97bc1454a90543c6117b00f
Rahul Heer <rahulheer344@gmail.com>
2025-10-03 16:11:27 +0530

    creating usermodel for userauthentication

--- server/config/db.js
+++ server/config/db.js
@@ -3,10 +3,9 @@ const mongoose = require('mongoose');
 const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
     });
     console.log('MongoDB connected');
+    console.log(process.env.MONGO_URI);
   } catch (error) {
     console.error('MongoDB connection failed:', error.message);
     process.exit(1);
