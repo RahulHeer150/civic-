a54d7191851114ae5c90611a30a9f245d72a1b0e
Rahul Heer <rahulheer344@gmail.com>
2025-10-04 09:57:09 +0530

    done

--- server/models/user.model.js
+++ server/models/user.model.js
@@ -3,4 +3,29 @@ const bcrypt=require('bcrypt')
 const jwt=require('jsonwebtoken')
 
 const userSchema=new mongoose.Schema({
-    
+    username:{
+        type:String,
+        required:true,
+    },
+    city:{
+        type:String,
+        required:true,
+
+    },
+    state:{
+
+    },
+    email:{
+
+    },
+    Phone_no:{
+
+    },
+    Password:{
+
+    },
+    Otp:{
+
+    }
+})
+
