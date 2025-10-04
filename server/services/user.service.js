25f23372dece39d8afa6caf05e4e33a9875cb532
Rahul Heer <rahulheer344@gmail.com>
2025-10-04 18:40:25 +0530

    user service done for registration functionality

--- server/services/user.service.js
+++ server/services/user.service.js
@@ -1,1 +1,20 @@
-const userModel=require
+const userModel=require('../models/user.model')
+
+module.exports.createUser=async({
+    username,
+})=>{
+    if(username||city||state||phone||email||password||otp){
+        throw new Error('All Fields are required to be filled!!!')
+    }
+    const user=userModel.create({
+        username,
+        city,
+        state,
+        phone,
+        email,
+        password,
+        otp
+
+    })
+    return user;
+}
