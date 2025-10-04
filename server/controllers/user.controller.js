7601ab111d2e0a7a3277268820e332f1d9d0f892
Rahul Heer <rahulheer344@gmail.com>
2025-10-04 18:47:20 +0530

    wokring on user controller

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -1,2 +1,12 @@
 const userModel=require('../models/user.model')
-const userService=require('.')
+const userService=require('../services/user.service')
+const { validationResult }=require('express-validator')
+const blackListTokenModel=require('../models/blacklist.model')
+
+module.exports.registerUser=async(req,res,next)=>{
+    const errors=validationResult(req);
+    if(!errors.isEmpty()){
+        return res.status(400).json({errors:errors.array()})
+    }
+    
+}
