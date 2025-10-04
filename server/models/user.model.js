efb3f8bca13bf3a42f29c4ddb0773936d577c843
Rahul Heer <rahulheer344@gmail.com>
2025-10-04 11:31:54 +0530

    usermodel done

--- server/models/user.model.js
+++ server/models/user.model.js
@@ -13,19 +13,53 @@ const userSchema=new mongoose.Schema({
 
     },
     state:{
+        type:String,
+        required:true,
+
 
     },
     email:{
+        type:String,
+        required:true,
+        unique:true,
+        lowercase:true,
+        minlength:[5,'email must be at least 5 characters long'],
+
 
     },
     Phone_no:{
-
+        type:String,
+        required:true,
+        unique:true,
+        
     },
     Password:{
+        type:String,
+        required:true,
+        unique:true,
+        minlength:[8,'password must be at least 8 characters long'],
 
     },
     Otp:{
+        type:String,
+        required:true,
+        unique:true,
 
     }
 })
 
+userSchema.methods.generateAuthToken=function(){
+    const token=jwt.sign({_id:this._id},process.env.JWT_SECRET_KEY,{expiresIn:'24h'})
+    return token
+}
+
+userSchema.methods.comaprePassword=async function(password){
+    return await bcrypt.compare(password,this.password)
+}
+
+userSchema.statics.hashPassword=async function(password){
+    return await bcrypt.hash(password,10)
+}
+
+const userModel=mongoose.model('user',userSchema);
+module.exports=userModel;
