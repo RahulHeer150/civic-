8ca4ca57868bb587e7446f9112722d0b0e063679
Rahul Heer <rahulheer344@gmail.com>
2025-10-09 22:06:55 +0530

    hero section ui done

--- client/src/components/Hero1.jsx
+++ client/src/components/Hero1.jsx
@@ -3,16 +3,25 @@ import heroImg from '../assets/herosec1.png'
 
 const Hero1 = () => {
    return (
-      <div className="flex flex-col min-h-screen pt-4 md:pt-10 ">
-      <div className="flex flex-grow flex-col-reverse md:flex-col lg:flex-row w-full max-w-[1190px] pt-8 md:pt-0 mx-auto justify-between items-center md:px-5 ">
+      <div className="flex flex-col min-h-screen pt-4 md:pt-10 bg-gradient-to-bl from-gray-300 to-sky-400">
+      <div className="flex flex-grow flex-col-reverse md:flex-col lg:flex-row w-full max-w-[1190px]  md:pt-0 mx-auto justify-between items-center md:px-5 ">
 
-        <div className="w-full lg:w-1/2 md:w-1/2 px-3 lg:px-0 text-black">
+        <div className="w-full lg:w-1/2 md:w-1/2  lg:px-0 text-black">
           <header className="relative h-full flex items-center justify-center">
             <div className="w-full p-[20px] text-start">
               <h1 className="text-7xl font-bold m-0 text-blue-700 justify-start">Having an Issue!</h1>
-              <h3 className="text-4xl mt-[10px] text-black justify-start font-bold">Report It.</h3>
+              <h3 className="text-5xl mt-[10px] text-black justify-start font-bold">Report It.</h3>
               <h3 className="text-4xl mt-[10px] text-black justify-start font-bold">Vote for Solutions.</h3>
+              <div className='py-5 flex gap-5'>
+               <button className='h-15 w-45 rounded-md bg-white text-blue-600 font-semibold text-xl '>
+                     Report an Issue
+               </button>
+               <button className='h-15 w-45 rounded-md bg-gray-400 text-black font-semibold text-xl '>
+                     Explore Issues
+               </button>
             </div>
+            </div>
+            
           </header>
         </div>
        <div className="w-full lg:w-1/2 md:w-1/2 flex jsutify-center items-center">
@@ -20,9 +29,8 @@ const Hero1 = () => {
             src={heroImg}
             alt="Report_header"
             height={350}
-            width={500}
             loading="lazy"
-            className="max-w-full h-auto md:px-5"
+            className="max-w-full  md:px-5"
           />
         </div>
       </div>
