95e3824e280964618628a3ffa0ab5be5e985193f
Rahul Heer <rahulheer344@gmail.com>
2025-10-15 10:40:45 +0530

    debugging for user routes backend

--- server/server.js
+++ server/server.js
@@ -15,7 +15,7 @@ app.get('/', (req, res) => {
   res.send('API is running...');
 });
 
-app.use('/api/auth', authRoutes);
+app.get('/api/auth', authRoutes);
 
 
 const PORT = process.env.PORT || 5000;
