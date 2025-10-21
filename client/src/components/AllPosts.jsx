13f94367d45f7e5c3c96a403443b8ef71afd75df
Rahul Heer <rahulheer344@gmail.com>
2025-10-21 16:32:15 +0530

    al posts section done

--- client/src/components/AllPosts.jsx
+++ client/src/components/AllPosts.jsx
@@ -4,7 +4,7 @@ const AllPosts = () => {
   return (
     <div className='h-screen w-screen bg-gray-400 justify-center items-center'>
         <div className='relative left-4 top-[200px]'>
-        <h1 className='font-sans font-semibold text-6xl text-black justify-center text-center'>
+        <h1 className='font-sans font-bold text-6xl text-gray-500 justify-center text-center'>
             No Issues Posted Here!
         </h1>
 
