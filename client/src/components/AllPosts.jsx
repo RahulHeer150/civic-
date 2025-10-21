92ab677ac7c9db08a7158a3eb3c3efeced4fec36
Rahul Heer <rahulheer344@gmail.com>
2025-10-21 15:56:41 +0530

    new component for individual posted issues

---
+++ client/src/components/AllPosts.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const AllPosts = () => {
+  return (
+    <div className='w-full bg-gray-300'>
+        <h1 className='font-sans font-semibold text-6xl text-gray-600'>
+            No Issues Posted Here!
+        </h1>
+
+    </div>
+  )
+}
+
+export default AllPosts;
