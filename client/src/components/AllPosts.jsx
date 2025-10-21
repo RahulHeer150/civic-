961032dd48913486f622531d156aeac96f174c54
Rahul Heer <rahulheer344@gmail.com>
2025-10-21 16:08:23 +0530

    allposts for individuals is done

--- client/src/components/AllPosts.jsx
+++ client/src/components/AllPosts.jsx
@@ -2,11 +2,14 @@ import React from 'react'
 
 const AllPosts = () => {
   return (
-    <div className='w-full bg-gray-300'>
-        <h1 className='font-sans font-semibold text-6xl text-gray-600'>
+    <div className='h-screen w-screen bg-gray-400 justify-center items-center'>
+        <div className='relative left-4 top-[200px]'>
+        <h1 className='font-sans font-semibold text-6xl text-black justify-center text-center'>
             No Issues Posted Here!
         </h1>
 
+        </div>
+        
     </div>
   )
 }
