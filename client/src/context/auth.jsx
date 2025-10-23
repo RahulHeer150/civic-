b1a8b97a8c9067ef9842e9ffdcf8a8957f2381b6
Rahul Heer <rahulheer344@gmail.com>
2025-10-23 12:23:24 +0530

    adding new context file

---
+++ client/src/context/auth.jsx
@@ -0,0 +1,64 @@
+import { createContext, useContext, useState, useEffect, Children } from "react";
+
+const URL=import.meta.env.CLIENT_URL;
+
+export const AuthContext=createContext();
+
+export const AuthProvider=({children})=>{
+    const [token,setToken]=useState(localStorage.getItem("token")|| "");
+    const [user, setUser] = useState(null);
+    const [isLoading, setIsLoading] = useState(true);
+    const authorizationToken = `Bearer ${token}`;
+
+    useEffect(() => {
+    localStorage.setItem("token", token);
+  }, [token]);
+
+  const storeTokenInLocalStorage = (serverToken) => {
+    setToken(serverToken);
+  };
+
+  const logoutUser = () => {
+    setToken("");
+    localStorage.removeItem("token");
+    setUser(null);
+  };
+
+   const userAuthentication = async () => {
+    setIsLoading(true);
+    try {
+      const response = await fetch(`${URL}/api/auth/user`, {
+        method: "GET",
+        headers: {
+          Authorization: authorizationToken,
+        },
+      });
+
+      if (response.ok) {
+        const data = await response.json();
+        setUser(data.userData);
+      } else {
+        console.error("Error fetching user data");
+      }
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    userAuthentication();
+  }, [authorizationToken]);
+
+  // Determine if the user is an admin based on the user data
+  const isAdmin = user && user.isAdmin === true;
+
+  const isLoggedIn = !!token;
+
+  return (
+    <AuthContext.Provider value={{ isLoggedIn, storeTokenInLocalStorage, logoutUser, user, authorizationToken, isLoading, isAdmin }}>
+      {children}
+    </AuthContext.Provider>
+  );
+}
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,14 +2,11 @@ import React from 'react'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import  UserContext  from './context/UserContext.jsx'
+import { AuthContext } from './context/auth.jsx'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
-
-  <StrictMode>
-    <UserContext>
+    <authContext>
     <App />
-    </UserContext>
-  </StrictMode>
+    </authContext>
 )
