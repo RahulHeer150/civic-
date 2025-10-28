81f6794e42e146c3b9d2ae281a85d5aeb6d9393d
Rahul Heer <rahulheer344@gmail.com>
2025-10-28 13:08:02 +0530

    code chnages for video image

--- client/src/components/Hero1.jsx
+++ client/src/components/Hero1.jsx
@@ -1,50 +1,50 @@
-import React from 'react'
-import heroImg from '../assets/herosec.jpg'
-import { Link, useNavigate } from 'react-router-dom'
+// import React from 'react'
+// import heroImg from '../assets/herosec.jpg'
+// import { Link, useNavigate } from 'react-router-dom'
 
-const Hero1 = () => {
-   // const submitHandler=(e)=>{
-   //    e.preventDefault();
+// const Hero1 = () => {
+//    // const submitHandler=(e)=>{
+//    //    e.preventDefault();
 
-   // }
+//    // }
 
-   return (
-      <div className="flex flex-col min-h-screen pt-4 md:pt-10 bg-gradient-to-bl from-gray-300 to-sky-300">
-      <div className="flex flex-grow flex-col-reverse md:flex-col lg:flex-row w-full max-w-screen  md:pt-0 m-5 justify-between items-center md:px-5 ">
+//    return (
+//       <div className="flex flex-col min-h-screen pt-4 md:pt-10 bg-gradient-to-bl from-gray-300 to-sky-300">
+//       <div className="flex flex-grow flex-col-reverse md:flex-col lg:flex-row w-full max-w-screen  md:pt-0 m-5 justify-between items-center md:px-5 ">
 
-        <div className="w-full lg:w-1/2 md:w-1/2  lg:px-0 text-black">
-          <header className="relative h-full flex items-center justify-center">
-            <div className="w-full p-[20px] text-start">
-              <h1 className="text-6xl font-bold m-0 text-blue-700 justify-start">Having an Issue!</h1>
-              <h3 className="text-5xl mt-[10px] text-black justify-start font-bold">Report It.</h3>
-              <h3 className="text-4xl mt-[10px] text-black justify-start font-bold">Vote for Solutions.</h3>
-              <div className='py-5 flex gap-5'>
-               <button
+//         <div className="w-full lg:w-1/2 md:w-1/2  lg:px-0 text-black">
+//           <header className="relative h-full flex items-center justify-center">
+//             <div className="w-full p-[20px] text-start">
+//               <h1 className="text-6xl font-bold m-0 text-blue-700 justify-start">Having an Issue!</h1>
+//               <h3 className="text-5xl mt-[10px] text-black justify-start font-bold">Report It.</h3>
+//               <h3 className="text-4xl mt-[10px] text-black justify-start font-bold">Vote for Solutions.</h3>
+//               <div className='py-5 flex gap-5'>
+//                <button
               
-               className='h-15 w-45 rounded-md bg-white text-blue-600 font-semibold text-xl '>
-                     Report an Issue
-               </button>
-               <button
+//                className='h-15 w-45 rounded-md bg-white text-blue-600 font-semibold text-xl '>
+//                      Report an Issue
+//                </button>
+//                <button
                
-               className='h-15 w-45 rounded-md bg-gray-300 text-black font-semibold text-xl '>
-                     Explore Issues
-               </button>
-            </div>
-            </div>
+//                className='h-15 w-45 rounded-md bg-gray-300 text-black font-semibold text-xl '>
+//                      Explore Issues
+//                </button>
+//             </div>
+//             </div>
             
-          </header>
-        </div>
-       <div className="w-full  flex jsutify-center items-center overflow-hidden ">
-          <img
-            src={heroImg}
-            alt="Report_header"
-            loading="lazy"
-            className="object-cover w-[1000px] h-full md:px-5  shadow-2xrounded-xl shadow-l-sky-300  overflow-hidden"
-          />
-        </div>
-      </div>
-    </div>
-    )
-}
+//           </header>
+//         </div>
+//        <div className="w-full  flex jsutify-center items-center overflow-hidden ">
+//           <img
+//             src={heroImg}
+//             alt="Report_header"
+//             loading="lazy"
+//             className="object-cover w-[1000px] h-full md:px-5  shadow-2xrounded-xl shadow-l-sky-300  overflow-hidden"
+//           />
+//         </div>
+//       </div>
+//     </div>
+//     )
+// }
 
-export default Hero1
+// export default Hero1
