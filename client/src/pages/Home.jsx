856be837e48843002cdfbca0e34465278a681ece
Rahul Heer <rahulheer344@gmail.com>
2025-10-30 23:15:33 +0530

    new code for new component for home

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -3,6 +3,7 @@ import Hero1 from '../components/Hero1'
 import Hero2 from '../components/Hero2'
 import Hero3 from '../components/Hero3'
 import OurMission from '../components/OurMission'
+import AboutHome from '../components/AboutHome'
 
 const Home = () => {
   return (
@@ -11,7 +12,9 @@ const Home = () => {
       <Hero1/>
       <Hero2/>
       <OurMission/>
+      <AboutHome/>
       <Hero3/>
+
 
     </div>
   )
