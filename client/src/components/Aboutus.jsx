cbafc4a75740fc7172fecd457ff88fa78a0087e4
Rahul Heer <rahulheer344@gmail.com>
2025-11-02 21:59:04 +0530

    new code changes

--- client/src/components/Aboutus.jsx
+++ client/src/components/Aboutus.jsx
@@ -1,54 +1,64 @@
-import React from 'react'
-import AboutImg from '../assets/abt.png'
+import React from "react";
+import AboutImg from "../assets/abt.png";
 
 const Aboutus = () => {
   return (
-      <div className='flex flex-col min-h-screen  p-10 mt-10 bg-[#d5def0]'>
-           <div className='flex flex-grow flex-col-reverse md:flex-row lg:flex-row w-full max-w-[1190px] pt-5 md:pt-0 mx-auto justify-between items-center '>
-             {/* left side image  */}
-             <div className='w-full lg:w-1/2 md:w-1/2 flex jsutify-center items-center'>
-              <img 
-              src={AboutImg} 
-              alt="forgot password" 
-              height={350}
-              width={500}
-              loading='lazy'
-              className='max-w-full h-auto hidden md:block lg:block'
-              />
-     
-             </div>
-     
-             {/* RIght side - Form  */} 
-             <div className='w-full lg:w-1/2 md:w-1/2 px-3 lg:px-0 md:px-5'>
-             <h2 className='text-4xl font-bold justify-center pb-5'>About CrowdFix</h2>
-             <div className="bg-[#f4f4f4] p-7 md:border-2 md:border-gray-700 md:rounded-xl md:shadow-2xl sm:border-2 sm:border-b-gray-700 sm:rounded-xl sm:shadow-2xl ">
-              
-
-        <div>
-          <p className='text-lg text-black '>
-            At <span>CrowdFix</span>, we believe in the power of community collaboration to create meaningful change. Founded in 2024, our mission is to empower citizens to take an active role in shaping neighborhoods by reporting local issues, voting on solutions, and tracking progress—all in one easy-to-use platform.
-          </p>
-
-          <p className='text-lg text-black '>
-            What sets CrowdFix apart is our commitment to transparency and innovation. Our platform bridges the gap between residents and local authorities, enabling faster resolutions and fostering trust through collective action. We are more than just a reporting tool—we are a movement that values collaboration, accountability, and collective effort.
-          </p>
-
-          <p className='text-lg text-black '>
-            Our journey began with a simple plan—collaboration, innovation, accountability—guiding everything we do. These principles ensure that our platform remains a trusted space where everyone, from individuals to organizations, can work together to make a difference.
-          </p>
-
-          <p className='text-lg text-black '>
-            Join us in shaping a better future. Whether you’re reporting a problem, voting on an issue, or exploring solutions, your participation matters.
-          </p>
+    <div className="flex flex-col min-h-screen  p-10 mt-10 bg-[#d5def0]">
+      <div className="flex flex-grow flex-col-reverse md:flex-row lg:flex-row w-full max-w-[1190px] pt-5 md:pt-0 mx-auto justify-between items-center ">
+        {/* left side image  */}
+        <div className="w-full lg:w-1/2 md:w-1/2 flex jsutify-center items-center">
+          <img
+            src={AboutImg}
+            alt="forgot password"
+            height={350}
+            width={500}
+            loading="lazy"
+            className="max-w-full h-auto hidden md:block lg:block"
+          />
         </div>
+
+        {/* RIght side - Form  */}
+        <div className="w-full lg:w-1/2 md:w-1/2 px-3 lg:px-0 md:px-5">
+          <h2 className="text-4xl font-bold justify-center pb-5">
+            About CrowdFix
+          </h2>
+          <div className="bg-[#f4f4f4] p-7 md:border-2 md:border-gray-700 md:rounded-xl md:shadow-2xl sm:border-2 sm:border-b-gray-700 sm:rounded-xl sm:shadow-2xl ">
+            <div>
+              <p className="text-lg text-black ">
+                At <span>CrowdFix</span>, we believe in the power of community
+                collaboration to create meaningful change. Founded in 2024, our
+                mission is to empower citizens to take an active role in shaping
+                neighborhoods by reporting local issues, voting on solutions,
+                and tracking progress—all in one easy-to-use platform.
+              </p>
+
+              <p className="text-lg text-black ">
+                What sets CrowdFix apart is our commitment to transparency and
+                innovation. Our platform bridges the gap between residents and
+                local authorities, enabling faster resolutions and fostering
+                trust through collective action. We are more than just a
+                reporting tool—we are a movement that values collaboration,
+                accountability, and collective effort.
+              </p>
+
+              <p className="text-lg text-black ">
+                Our journey began with a simple plan—collaboration, innovation,
+                accountability—guiding everything we do. These principles ensure
+                that our platform remains a trusted space where everyone, from
+                individuals to organizations, can work together to make a
+                difference.
+              </p>
+
+              <p className="text-lg text-black ">
+                Join us in shaping a better future. Whether you’re reporting a
+                problem, voting on an issue, or exploring solutions, your
+                participation matters.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
-             
-     
-             </div>
-             
-           </div>
-     
-         </div>
-    )
-}
-export default Aboutus
+  );
+};
+export default Aboutus;
