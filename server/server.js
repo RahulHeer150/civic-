0b41d255abba0b4d8212eda228ecae6dd1b9ee48
Rahul Heer <rahulheer344@gmail.com>
2025-11-16 19:33:35 +0530

    new code for multer chnages

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const cors = require('cors');
+const path = require('path');
 const connectDB = require('./config/db');
 const authRoutes = require('./routes/auth.routes');
 const issueRoutes=require('./routes/issue.routes')
@@ -12,6 +13,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')))
 
 // ✅ Connect to DB
 connectDB();
@@ -22,7 +24,10 @@ app.get('/', (req, res) => {
 });
 
 // ✅ Mount routes
+
 app.use('/users', authRoutes);
+
+
 app.use("/issues",issueRoutes)
 
 const PORT = process.env.PORT || 5001;
