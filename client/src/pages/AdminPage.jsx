4a4c6946a25bfe702da4f1cec0894aa7364b6513
Rahul Heer <rahulheer344@gmail.com>
2025-11-18 22:47:45 +0530

    new sample code for displaying issues to admin

--- client/src/pages/AdminPage.jsx
+++ client/src/pages/AdminPage.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const AdminPage = () => {
   return (
-    <div>AdminPage</div>
+    <div className='w-screen h-screen mx-5 my-5 p-5 bg-black rounded-xl '>
+        <h1 className='text-white  text-center justify-center font-4xl'>hello</h1>
+    </div>
   )
 }
 
