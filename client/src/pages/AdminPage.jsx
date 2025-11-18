5f05831fadc8417561fc88d57b5ce7a21e4444eb
Rahul Heer <rahulheer344@gmail.com>
2025-11-18 22:52:46 +0530

    basic code for admin page

--- client/src/pages/AdminPage.jsx
+++ client/src/pages/AdminPage.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const AdminPage = () => {
   return (
-    <div className='w-screen h-screen mx-5 my-5 p-5 bg-black rounded-xl '>
+    <div className='max-w-screen h-screen mx-10 mt-20 p-5 bg-black rounded-xl '>
         <h1 className='text-white  text-center justify-center font-4xl'>hello</h1>
     </div>
   )
