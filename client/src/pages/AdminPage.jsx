7f11048d562124dd13f69bcc0cb680bd15c9621c
Rahul Heer <rahulheer344@gmail.com>
2025-11-19 22:38:38 +0530

    new code changes

--- client/src/pages/AdminPage.jsx
+++ client/src/pages/AdminPage.jsx
@@ -2,17 +2,21 @@ import React from 'react'
 
 const AdminPage = () => {
   return (
-    <div className='max-w-screen h-screen mx-10 mt-20 p-5 bg-gray-300 rounded-xl '>
-      <div className='w-full px-5 py-5 flex h-10 bg-white rounded-xl'>
-        <div>
-
+    <div className='max-w-screen h-screen mx-10 mt-20 p-5 bg-gray-300 rounded-xl items-center'>
+      <h1 className='font-bold text-gray-800 text-4xl text-center py-5'>ALL Reported Issues</h1>
+      <div className='w-1/2 px-5 py-3  flex h-15 bg-white rounded-xl relative left-20'>
+            <div className='h-10 w-10 rounded-full bg-blue-400'>
+              img
+            </div>
+          <h1 className='text-center px-5 text-xl  '>
+            Issue Title
+          </h1>
         </div>
         <div className=''>
 
         </div>
 
       </div>
-    </div>
   )
 }
 
