3e4a4ae29a9748c09ebbdd5f12f6080b4a2b9613
Rahul Heer <rahulheer344@gmail.com>
2025-11-19 22:59:18 +0530

    ui for admin

--- client/src/pages/AdminPage.jsx
+++ client/src/pages/AdminPage.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
-import Link from "react-router-dom"
+import {Link} from "react-router-dom"
 
 const AdminPage = () => {
   return (
     <div className='max-w-screen h-screen mx-10 mt-20 p-5 bg-gray-300 rounded-xl items-center justify-center '>
       <h1 className='font-bold text-gray-800 text-4xl text-center py-5'>ALL Reported Issues</h1>
       
-        <div className='w-full p-5 border-2 border-gray-500 rounded-xl '>
+        <div className='w-full p-5 border-2 border-gray-500 rounded-xl flex '>
           <div className='w-1/2 px-5 py-3  flex h-15 bg-white rounded-xl '>
             <div className='h-10 w-10 rounded-full bg-blue-400'>
               img
@@ -15,11 +15,11 @@ const AdminPage = () => {
             Issue Title
           </h1>
         </div>
-        <div className='px-10 py-3'>
-          <Link className="bg-blue-400 h-10 w-30">
+        <div className='w-1/2 px-10 py-4 justify-end items-end mx-20'>
+          <Link className="bg-blue-400 h-10 w-30 px-10 py-3 text-lg text-white font-semibold rounded-xl gap-10 mr-5">
             Resolve
           </Link>
-          <Link className="bg-gray-400 h-10 w-30">
+          <Link className="bg-gray-400 h-10 w-30 px-10 py-3 text-lg text-white font-semibold rounded-xl">
             View
           </Link>
 
