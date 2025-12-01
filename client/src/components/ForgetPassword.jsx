03e167dfc0e52bb24162cf4b74949c55de0cab00
Rahul Heer <rahulheer344@gmail.com>
2025-12-01 17:13:47 +0530

    new code chnage

--- client/src/components/ForgetPassword.jsx
+++ client/src/components/ForgetPassword.jsx
@@ -17,7 +17,7 @@ const ForgetPassword = () => {
     setLoading(true);
 
     try {
-      const response = await fetch("http://localhost:5000/api/auth/forgot-password", {
+      const response = await fetch("http://localhost:5001/users/forgot-password", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
