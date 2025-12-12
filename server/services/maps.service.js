f4d5117287b32f78188fba122c9fe72e8216d6ec
Rahul Heer <rahulheer344@gmail.com>
2025-12-12 14:33:47 +0530

    new code updation for storing exact address

--- server/services/maps.service.js
+++ server/services/maps.service.js
@@ -1,21 +1,59 @@
+// const axios = require("axios");
+
+// const getAddressFromCoordinates = async (lat, lng) => {
+//   try {
+//     const response = await axios.get(
+//       `https://maps.googleapis.com/maps/api/geocode/json?latlng=${lat},${lng}&key=${process.env.GOOGLE_MAPS_API_KEY}`
+//     );
+
+//     if (response.data.results.length > 0) {
+//       return response.data.results[0].formatted_address;
+//     }
+
+//     return "Unknown Location";
+
+//   } catch (error) {
+//     console.error("Geocoding error:", error.message);
+//     return "Location Unavailable";
+//   }
+// };
+
+// module.exports = getAddressFromCoordinates;
 const axios = require("axios");
 
 const getAddressFromCoordinates = async (lat, lng) => {
   try {
+    const apiKey = process.env.GOOGLE_MAPS_API_KEY;
+
     const response = await axios.get(
-      `https://maps.googleapis.com/maps/api/geocode/json?latlng=${lat},${lng}&key=${process.env.GOOGLE_MAPS_API_KEY}`
+      `https://maps.googleapis.com/maps/api/geocode/json?latlng=${lat},${lng}&key=${apiKey}`
     );
 
-    if (response.data.results.length > 0) {
-      return response.data.results[0].formatted_address;
-    }
+    const result = response.data.results[0];
+    if (!result) return {};
 
-    return "Unknown Location";
+    let city = "";
+    let state = "";
+    let postalCode = "";
+
+    result.address_components.forEach((comp) => {
+      if (comp.types.includes("locality")) city = comp.long_name;
+      if (comp.types.includes("administrative_area_level_1")) state = comp.long_name;
+      if (comp.types.includes("postal_code")) postalCode = comp.long_name;
+    });
+
+    return {
+      address: result.formatted_address,
+      city,
+      state,
+      postalCode
+    };
 
   } catch (error) {
     console.error("Geocoding error:", error.message);
-    return "Location Unavailable";
+    return {};
   }
 };
 
 module.exports = getAddressFromCoordinates;
+
