659854054fc48e22fb868fff2d0b530afe7e8e95
Rahul Heer <rahulheer344@gmail.com>
2025-12-17 19:22:33 +0530

    new code updations

--- client/src/components/Logout.jsx
+++ client/src/components/Logout.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { useEffect } from "react";
 import { useAuth } from "../context/auth";
-import { useNavigate } from "react-router-dom";
+import { Navigate,useNavigate } from "react-router-dom";
 import { toast } from "react-toastify";
 export const Logout = () => {
   const { logoutUser } = useAuth(); // Use 'logoutUser' instead of 'LogoutUser'
