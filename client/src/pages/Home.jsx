3b5b859333a107a2d0cc018e30df7f79d04898ec
Rahul Heer <rahulheer344@gmail.com>
2026-01-19 22:46:38 +0530

    new how it wroks code chnags

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,23 +1,20 @@
-import React from 'react'
-import Hero1 from '../components/Hero1'
-import Hero2 from '../components/Hero2'
-import Hero3 from '../components/Hero3'
-import OurMission from '../components/OurMission'
-import AboutHome from '../components/AboutHome'
+import React from "react";
+import Hero1 from "../components/Hero1";
+import Hero2 from "../components/Hero2";
+import Hero3 from "../components/Hero3";
+import OurMission from "../components/OurMission";
+import AboutHome from "../components/AboutHome";
 
 const Home = () => {
   return (
     <div>
-      
-      <Hero1/>
-      <Hero2/>
-      <OurMission/>
-      <AboutHome/>
-      <Hero3/>
-
-
+      <Hero1 />
+      <Hero2 />
+      <OurMission />
+      <AboutHome />
+      <Hero3 />
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
