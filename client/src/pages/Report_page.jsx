74130f86ab2ee41e02cd50ee82f7d8ef98f7adf5
Rahul Heer <rahulheer344@gmail.com>
2026-01-19 22:43:51 +0530

    new code updations

--- client/src/pages/Report_page.jsx
+++ client/src/pages/Report_page.jsx
@@ -1,14 +1,14 @@
-import React from 'react'
-import Report_Header from '../components/Report_Header'
-import ReportForm from '../components/ReportForm'
+import React from "react";
+import Report_Header from "../components/Report_Header";
+import ReportForm from "../components/ReportForm";
 
 const Report_page = () => {
   return (
     <div>
-      <Report_Header/>
-      <ReportForm/>
+      <Report_Header />
+      <ReportForm />
     </div>
-  )
-}
+  );
+};
 
-export default Report_page
+export default Report_page;
