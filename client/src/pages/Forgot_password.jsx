ada40cf7b3754d547f182895031aea68f9d2fb68
Rahul Heer <rahulheer344@gmail.com>
2026-01-19 22:46:26 +0530

    new code changes

--- client/src/pages/Forgot_password.jsx
+++ client/src/pages/Forgot_password.jsx
@@ -1,23 +1,23 @@
-import React from 'react'
-
+import React from "react";
 
 const Forgot_password = () => {
   return (
-    <div className='flex flex-col min-h-screen pt-4 md:pt-20'>
-      <div className='flex-grow flex flex-col-reverse md:flex-row lg:flex-row w-full max-w-[1160px] pt-16 md:pt-0 mx-auto justify-between items-center mt-[12vh]'>
+    <div className="flex flex-col min-h-screen pt-4 md:pt-20">
+      <div className="flex-grow flex flex-col-reverse md:flex-row lg:flex-row w-full max-w-[1160px] pt-16 md:pt-0 mx-auto justify-between items-center mt-[12vh]">
         {/* leftside img */}
-        <div className='w-full lg:w-1/2 md:w-1/2 flex justify center items-center '>
-        <img src={loginImg} 
-        alt="pattren"
-        width={500}
-        height={350}
-        loading='lazy'
-        className='max-w-full h-auto hidden md:block lg:block' />
+        <div className="w-full lg:w-1/2 md:w-1/2 flex justify center items-center ">
+          <img
+            src={loginImg}
+            alt="pattren"
+            width={500}
+            height={350}
+            loading="lazy"
+            className="max-w-full h-auto hidden md:block lg:block"
+          />
         </div>
       </div>
-
     </div>
-  )
-}
+  );
+};
 
-export default Forgot_password
+export default Forgot_password;
