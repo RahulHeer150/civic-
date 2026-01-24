73a7840abad1a99d2c2211298c692275d7fc2b32
Rahul Heer <rahulheer344@gmail.com>
2026-01-24 22:39:49 +0530

    new code hnages

--- client/src/context/UserContext.jsx
+++ client/src/context/UserContext.jsx
@@ -1,28 +1,4 @@
-// import React, { createContext } from 'react'
-// import { useState } from 'react'
 
-// export const UserDataContext=createContext()
-
-// const UserContext = ({children}) => {
-//     const [user, setUser] = useState({
-
-//         email:'',
-//         Username:'',
-//         City:'',
-//         State:'',
-//         phone:'',
-//         password:'',
-//         otp:''
-//     })
-//   return (
-
-//         <UserDataContext.Provider value={{user,setUser}}>
-//             {children}
-//         </UserDataContext.Provider>
-//     )
-// }
-
-// export default UserContext
 
 // ...existing code...
 import React, { createContext, useState } from "react";
