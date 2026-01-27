863baf691716c430df797958798e53f3197c979e
Rahul Heer <rahulheer344@gmail.com>
2026-01-27 15:55:28 +0530

    new code changes

--- server/routes/auth.routes.js
+++ server/routes/auth.routes.js
@@ -1,20 +1,19 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const userController=require("../controllers/auth.controller")
-const authMiddleware = require('../middlewares/auth.middleware');
+const userController = require("../controllers/auth.controller");
+const authMiddleware = require("../middlewares/auth.middleware");
 
 // ✅ Register User (POST)
-router.post('/register', userController.register);
+router.post("/register", userController.register);
 
-router.post('/login', userController.loginUser)
+router.post("/login", userController.loginUser);
 
 // ✅ Get Profile (GET)
-router.get('/profile',authMiddleware.authUser, userController.getUserProfile);
+router.get("/profile", authMiddleware.authUser, userController.getUserProfile);
 
-router.get('/logout',authMiddleware.authUser,userController.logoutUser)
+router.get("/logout", authMiddleware.authUser, userController.logoutUser);
 
-
-router.post("/forgot-password",userController.forgotPassword);
+router.post("/forgot-password", userController.forgotPassword);
 router.post("/reset-password/:token", userController.resetPassword);
 
 module.exports = router;
