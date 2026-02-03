2c8875997c69db30b6bc27234ad4c199720d3544
Rahul Heer <rahulheer344@gmail.com>
2026-02-03 20:33:39 +0530

    new code chnages

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -130,7 +130,7 @@ module.exports.getUserProfile = async (req, res, next) => {
     });
   }
 };
-
+\
 module.exports.logoutUser = async (req, res, next) => {
   try {
     // safe optional chaining for cookies and headers
