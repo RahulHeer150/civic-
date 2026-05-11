8f6f6bcb26125f3c51faa6ec85bb5a16de0e3ee5
Rahul Heer <rahulheer344@gmail.com>
2026-05-11 23:06:26 +0530

    new code updations for servr

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -55,7 +55,6 @@ module.exports.register = async (req, res) => {
     
   
 
-
     const isUserAlready = await userModel.findOne({ email });
     if (isUserAlready) {
       return res.status(400).json({ message: "User already exists" });
