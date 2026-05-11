f159fb28878b21121ad0f98c5d3ed6abfa809e19
Rahul Heer <rahulheer344@gmail.com>
2026-05-11 23:10:22 +0530

    new code chnages

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -56,7 +56,7 @@ module.exports.register = async (req, res) => {
 
 
 
-    
+  
     const isUserAlready = await userModel.findOne({ email });
     if (isUserAlready) {
       return res.status(400).json({ message: "User already exists" });
